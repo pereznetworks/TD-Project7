b9545b509000429e6a4caa9445c1ed49e0073e83
Daniel Perez <pereznetworks@live.com>
2018-08-22 09:05:47 -0700

    now generating dynamic working projects route
    now generating dynamic working projects route
    
    not to plug in object references to data

--- app.js
+++ app.js
@@ -1,70 +1,52 @@
 // app.js - main js for Static Porfolio Site to show off my projets
 
+// require express framework module and create an express app object
+    const express = require('express');
+    const app = express();
+
+// require and import project and profile data
+    const portfolio = {}
+    portfolio.projects = require('./data.json').projects;
+    portfolio.profile = require('./profile.json').profile;
+
 // encapsulating html snippets, status vars and a log method ...
 // will use this to setup error handling
 // will need to setup pug template to render html snippets
     const message = require('./routes/message.js');
 
-// require express framework module and create an express app object
-    const express = require('express');
-    const app = express();
-// require project and profile data
-    const portfolio = {}
-    portfolio.projects = require('./data.json').projects;
-    portfolio.profile = require('./profile.json').profile;
-
-
 // pug, .set method, view engine, beginning to implement html templates ...
     app.set('view engine', 'pug');
+
 // set static stylesheets and html rendering engine
     app.use('/static', express.static('public'));
     app.use('/img', express.static('img'));
 
-// //import and use express routes
-//     const mainRoutes = require('./routes');
-//     app.use(mainRoutes);
-
-// // custom error handling
-//     app.use((req, res, next) => {  // custom 404 not found page
-//         const err = new Error('Not Found');
-//         err.status = 404;
-//         next(err);
-//     });
-//
-//     app.use((err, req, res, next) => { // custom error handler
-//         res.locals.error = err;
-//         res.status(err.status);
-//         res.render('error');
-//     });
-
-// using the .get method to setup simple routes
+// route for main page
     app.get('/', (req, res, next) => {
         res.render( 'index', portfolio);
-        // { profile: [{heading: 'My Cool Apps', portfolio_desc: 'A portfolio of my cool apps'}]}
-        console.log(portfolio.profile[0].heading);
-        console.log(portfolio.profile[0].portfolio_desc);
-        console.log(portfolio.projects[0].project_name);
-        console.log(portfolio.projects[0].description);
-        console.log(portfolio.projects[0].image_urls[0]);
         next();
-
     });
 
+// route for about page
     app.get('/about', (req, res, next) => {
         res.render( 'about' );
         message.log( message.status.about );
         next();
     });
 
+// capturing the id passed
+// for id of project details to render using project.pug
     app.param('id', (req, res, next, id) => {
+        message.log( message.status.projects );
+        console.log(portfolio.projects[id].project_name);
         res.render( 'project', portfolio.projects[id] );
-        message.log( message.status.projects );
-        next();
-    });
+        next(id);
+    })
 
-    app.get('/projects', (req, res, next) => {
-        res.render( 'project' );
-        message.log( message.status.projects );
+// route for projects
+    app.get('/projects/:id', (req, res, next, id) => {
+        // this route triggers app.param(id)
+        // the rendering and messaging happens in app.param callback function
         next();
     });
 
@@ -72,3 +54,30 @@
     app.listen(3000, () => {
         message.log( message.status.running );
     });
+
+/* NOTE: just some additional testing and code I may or may not use
+
+    // { profile: [{heading: 'My Cool Apps', portfolio_desc: 'A portfolio of my cool apps'}]}
+    // console.log(portfolio.profile[0].heading);
+    // console.log(portfolio.profile[0].portfolio_desc);
+    // console.log(portfolio.projects[0].project_name);
+    // console.log(portfolio.projects[0].description);
+    // console.log(portfolio.projects[0].image_urls[0]);
+
+    // // import and use express routes
+    //     const mainRoutes = require('./routes');
+    //     app.use(mainRoutes);
+
+    // // custom error handling
+    //     app.use((req, res, next) => {  // custom 404 not found page
+    //         const err = new Error('Not Found');
+    //         err.status = 404;
+    //         next(err);
+    //     });
+    //
+    //     app.use((err, req, res, next) => { // custom error handler
+    //         res.locals.error = err;
+    //         res.status(err.status);
+    //         res.render('error');
+    //     });
+    */
