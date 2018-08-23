d2df1f0ae1b91e86f05a7a667a019e79e42ff78a
Daniel Perez <pereznetworks@live.com>
2018-08-23 12:37:13 -0700

    ok, valid paths no longer produce errors
    ok, valid paths, static resources and pages, no longer produce errors
    
    have to re-think error handling,

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static(path.join('./', 'public')));
 app.use('/img', express.static(path.join('./','img')));
+app.use('/projects/static/', express.static(path.join('./', 'public')));
 
 app.get('/', (req, res, next) => {
   res.render( 'index', portfolio);
-  next();
+  next('route');
 });
 
 app.get('/about', (req, res, next) => {
   message.log( message.status.about );
   res.render( 'about', portfolio.profile[0]);
-  next();
+  next('route');
 });
 
 app.param('id', (req, res, next, id) => {
@@ -29,37 +30,33 @@ app.param('id', (req, res, next, id) => {
       message.log( message.status.projects);
       console.log(portfolio.projects[id].project_name);
       res.render( 'project', portfolio.projects[id] );
-      next();
-  } else {
-    next(err);
   }
-})
+});
 
 app.get('/projects/:id', (req, res, next, id) => {
-  // const x = portfolio.projects.length - 1;
-  const validProjectsId = /[/projects]\w+/[0-4]/g
-  if (req.url.match(validProjectsId)){
-     next();
-  } else {
-    next(err);
-  }
+  // need this route here to trigger app.param('id') handler
+   next('route');
 });
 
 app.get('/projects', (req, res, next) => {
   res.render( 'projects', portfolio);
-  next();
+  next('route');
 });
 
-app.get((req, res, next) => {
-  const err = new Error(`Oops, excuse us, we can't find that page or path`);
-  next(err);
-});
+// app.use((req, res, next) => {
+//     const err = new Error(`Oops, excuse us, we can't find that page or path`);
+//     err.status = `404`;
+//     err.msg = `It seems we can't find that page or path`;
+//     next(err);
+// });
 
 app.use((err, req, res, next) => {
-  err.status = `Excuse us! We're so sorry.`;
-  err.msg = `It seems we can't that page or path`;
-  console.log(`\nerror is: ${err.status}\nmessage is: ${err.msg}\n`);
-  res.render('error', err);
+    console.log(`path requested: ${req.url}\nerror is: ${err.status}\nmessage is: ${err.msg}\n`);
+    err = new Error(`Oops, excuse us, we can't find that page or path`);
+    err.status = `404`;
+    err.msg = `It seems we can't find that page or path`;
+    res.status(err.status);
+    res.render('error', err);
 });
 
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,34 @@
-// creating an express router
-  const express = require('express');
-  router = express.Router();
-
-// no, not really using this right now
+// // creating an express router
+// const express = require('express');
+// router = express.Router();
+//
+// // routes
+//
+//   router.get('/', (req, res) => {
+//     res.render( 'index', portfolio);
+//   });
+//
+//   router.get('/about', (req, res) => {
+//     message.log( message.status.about );
+//     res.render( 'about', portfolio.profile[0]);
+//   });
+//
+//   router.param('id', (req, res, id) => {
+//     if (id < portfolio.projects.length ) {
+//         message.log( message.status.projects);
+//         console.log(portfolio.projects[id].project_name);
+//         res.render( 'project', portfolio.projects[id] );
+//     }
+//   });
+//
+//   router.get('/projects/:id', (req, res, id) => {
+//     // this triggers router.param
+//   });
+//
+//   router.get('/projects', (req, res) => {
+//     res.render( 'projects', portfolio);
+//   });
+//
+// // exporting router so it can be used by express' app.js
+//
+// module.exports = router;
